4366eb7f545f295a043c655b10ae262f2709575f
pboennig <pboennig@gmail.com>
2016-01-31 01:22:42 -0800

    Changed shit (literally)

--- model/OptimalValuesCalculator.java
+++ model/OptimalValuesCalculator.java
@@ -2,20 +2,19 @@ public class OptimalValuesCalculator {
     
     private double landArea;
     private Crop crop;                // make enum
-    private Fertilizer fertilizer;
-    private double precipitationPerHectacre;
+    private double nFertilizerContent;
 
     public static void main(String... args) {
-        OptimalValuesCalculator myCalc = new OptimalValuesCalculator(5.0, Crop.CORN, Fertilizer.COW);
+        OptimalValuesCalculator myCalc = new OptimalValuesCalculator(5.0, Crop.CORN, .72);
         System.out.println(myCalc.getWaterAmount()); // in liters
         System.out.println(myCalc.getFertilizerAmount()); // in kg
         System.out.println(myCalc.getSeedAmount()); //in seeds
     }
     
-    public OptimalValuesCalculator(double landArea, Crop crop, Fertilizer fertilizer) {
+    public OptimalValuesCalculator(double landArea, Crop crop, int nFertilizerContent) {
         this.landArea = landArea; // in m^2
         this.crop = crop; //type of crop [Corn, Rice, Wheat, Soybean]
-        this.fertilizer = fertilizer; //type of fertilizer [Urea, Sheep, Swine, Cow, Chicken]
+        this.nFertilizerContent = nFertilizerContent; //type of fertilizer [Urea, Sheep, Swine, Cow, Chicken]
     }
 
     public double getSeedAmount() {
@@ -23,7 +22,7 @@ public class OptimalValuesCalculator {
     }
     
     public double getFertilizerAmount() {
-        return this.crop.getOptimalFertilizer() / this.fertilizer.getNPercentage() / 100000; // to account for m^2 to hectares, in kg
+        return this.crop.getOptimalFertilizer() / this.nFertilizerContent / 100000; // to account for m^2 to hectares, in kg
     }
     
     /**
